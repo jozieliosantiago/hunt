cbad97b68681c2a7f4016e5d8f3d1caac71bf169
Joziélio Santiago <jozieliosantiago@gmail.com>
2019-07-23 18:56:25 -0300

    service api added

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
   static navigationOptions = {
     title: "JSHunt",
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  };
+
+  loadProducts = async () => {
+    const response = await api.get('/products');
+    const { docs } = response.data;
   };
 
   render() {
