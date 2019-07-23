6df4ddf2f0b492d4c78501c14b0c9f20c2c74ba2
Joziélio Santiago <jozieliosantiago@gmail.com>
2019-07-22 23:25:37 -0300

    react-navigation added

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, Text, } from 'react-native';
+import Routes from './routes';
+import './config/StatusBarConfig';
+// import { StyleSheet, View, Text, } from 'react-native';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>React Native</Text>
-    </View>
-  );
-};
+const App = () => <Routes />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20
-  }
-});
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>React Native</Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   title: {
+//     fontSize: 20
+//   }
+// });
 
 export default App;
